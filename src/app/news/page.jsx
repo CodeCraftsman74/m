e852a0c39886dcd2c1a3509eca2f72d337bcdac3
CodeCraftsman74 <150464550+CodeCraftsman74@users.noreply.github.com>
2025-05-06 05:00:42 +0530

    updates

--- src/app/news/page.jsx
+++
@@ -1,9 +0,0 @@
-import NewsFeed from '@/components/NewsFeed/NewsFeed';
-
-export default function NewsPage() {
-  return (
-    <div className="container mx-auto py-8">
-      <NewsFeed />
-    </div>
-  );
-} 
--- src/app/quiz/page.jsx
+++
@@ -1,9 +0,0 @@
-import QuizComponent from '@/components/Quiz/QuizComponent';
-
-export default function QuizPage() {
-  return (
-    <div className="container mx-auto py-8">
-      <QuizComponent />
-    </div>
-  );
-} 
